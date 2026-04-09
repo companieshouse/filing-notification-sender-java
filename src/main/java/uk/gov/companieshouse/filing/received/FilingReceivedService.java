1a509527f6d71c6e47a5773fb16eab20caea4161
cgregoryCH <cgregory@companieshouse.gov.uk>
2026-04-09 18:52:05 +0100

    KAF-120: Implement filing received service
    * Does not include mapping/business logic.
    * Retry behaviour needs revisited.

--- src/main/java/uk/gov/companieshouse/filing/received/FilingReceivedService.java
+++ src/main/java/uk/gov/companieshouse/filing/received/FilingReceivedService.java
@@ -1,13 +1,32 @@
 package uk.gov.companieshouse.filing.received;
 
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.api.chskafka.MessageSend;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.filing.common.Service;
+import uk.gov.companieshouse.filing.common.client.KafkaApiClient;
+import uk.gov.companieshouse.filing.common.client.TransactionsApiClient;
 
 @Component
 public class FilingReceivedService implements Service<FilingReceived> {
 
+    private final TransactionsApiClient transactionsApiClient;
+    private final FilingReceivedMapper filingReceivedMapper;
+    private final KafkaApiClient kafkaApiClient;
+
+    public FilingReceivedService(TransactionsApiClient transactionsApiClient, FilingReceivedMapper filingReceivedMapper,
+            KafkaApiClient kafkaApiClient) {
+        this.transactionsApiClient = transactionsApiClient;
+        this.filingReceivedMapper = filingReceivedMapper;
+        this.kafkaApiClient = kafkaApiClient;
+    }
+
     @Override
     public void handlePayload(FilingReceived payload) {
-        // handle payload
+        Transaction transaction = transactionsApiClient.getTransaction(payload.getSubmission().getTransactionId());
+        payload.getItems().forEach(item -> {
+            MessageSend messageSend = filingReceivedMapper.map(payload, item, transaction);
+            kafkaApiClient.postMessageSend(messageSend);
+        });
     }
 }
--- src/test/java/uk/gov/companieshouse/filing/received/ConsumerRetryableExceptionIT.java
+++ src/test/java/uk/gov/companieshouse/filing/received/ConsumerRetryableExceptionIT.java
@@ -1,24 +1,12 @@
 package uk.gov.companieshouse.filing.received;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.anyRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
-import static com.github.tomakehurst.wiremock.client.WireMock.verify;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import uk.gov.companieshouse.filing.common.Service;
-import uk.gov.companieshouse.filing.common.exception.RetryableException;
 
 @SpringBootTest
 class ConsumerRetryableExceptionIT extends AbstractFilingReceivedConsumerIT {
-
-    @MockitoBean
-    private Service<FilingReceived> service;
 
     @DynamicPropertySource
     static void props(DynamicPropertyRegistry registry) {
@@ -26,17 +14,35 @@ class ConsumerRetryableExceptionIT extends AbstractFilingReceivedConsumerIT {
     }
 
     @Test
-    void testRepublishToErrorTopicThroughRetryTopics() throws Exception {
+    void testRepublishToErrorTopicThroughRetryTopicsWhenTransactionsApiErrorResponse() throws Exception {
         // given
         byte[] message = writePayloadToBytes(buildFilingReceived(), FilingReceived.class);
 
-        doThrow(RetryableException.class).when(service).handlePayload(any());
+        stubTransactionsApiResponse(500);
 
         // when
         publishAndAwaitConsumerLatch(message, 30);
 
         // then
         assertExpectedRecordsPerTopic(4, 1, 0);
-        verify(0, anyRequestedFor(anyUrl()));
+        verifyTransactionsApiRequest(5);
+        verifyKafkaApiRequest(0);
+    }
+
+    @Test
+    void testRepublishToErrorTopicThroughRetryTopicsWhenKafkaApiErrorResponse() throws Exception {
+        // given
+        byte[] message = writePayloadToBytes(buildFilingReceived(), FilingReceived.class);
+
+        stubTransactionsApiResponse(200);
+        stubKafkaApiResponse(500);
+
+        // when
+        publishAndAwaitConsumerLatch(message, 30);
+
+        // then
+        assertExpectedRecordsPerTopic(4, 1, 0);
+        verifyTransactionsApiRequest(5);
+        verifyKafkaApiRequest(5);
     }
 }
